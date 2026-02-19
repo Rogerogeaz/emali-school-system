02589c71091bcf02261b8e0816dcfac44d688fe8
Rogerogeaz <rogerogeaz@gmail.com>
2026-02-19 18:47:00 +0300

    Add caching functionality to service worker
    Implement caching for service worker installation and fetch events.

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,32 @@
-self.addEventListener("install", function (event) {
-  console.log("Service Worker Installed");
+const CACHE_NAME = "emali-school-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png"
+];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", function (event) {
-  console.log("Service Worker Activated");
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    self.clients.claim()
+  );
 });
 
-self.addEventListener("fetch", function (event) {
-  // You can add caching later
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+  );
 });
